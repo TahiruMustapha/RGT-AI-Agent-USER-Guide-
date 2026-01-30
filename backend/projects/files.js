999094cf7df9123f978671de04eb51bf6664ecad
BraCoded02024 <owusubernard050931@gmail.com>
2026-01-30 17:15:17 +0000

    Reading of files,calling files exactly as sring ad checking error when reading files

---
+++ backend/projects/files.js
@@ -0,0 +1,24 @@
+//reading files
+const fs = require('fs');
+fs.readFile('./docs/blog1.txt',(err,data)=>{
+
+if(err){
+console.log(err);
+}
+console.log(data.toString());
+
+})
+
+
+
+
+//writing files
+
+
+//directories
+
+
+
+
+
+//deleting files a
