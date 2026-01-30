17d07878c8241a869e76dcf9607068e80e73f225
BraCoded02024 <owusubernard050931@gmail.com>
2026-01-30 16:17:42 +0000

    first commit

---
+++ backend/projects/hello-world.js
@@ -0,0 +1,12 @@
+const call = require('./people');
+     
+console.log(call.kelvin,call.ages );
+
+
+
+const os = require('os');
+console.log(os);
+
+console.log(os.platform()), os.homedir();
+
+
---
+++ backend/projects/people.js
@@ -0,0 +1,12 @@
+const kelvin = ['Alice', 'Bob', 'Charlie', 'David'];
+const ages = [25, 30, 35, 40];
+
+
+module.exports = { 
+    kelvin,ages
+ };
+
+// console.log("hello world");
+
+
+
