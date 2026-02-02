8b238abf4592a7b1091ffa4a31401c86ffea852d
BraCoded02024 <owusubernard050931@gmail.com>
2026-02-02 13:59:54 +0000

    Reading chunky and stream data

--- backend/projects/files.js
+++ backend/projects/files.js
@@ -1,33 +1,60 @@
 //Writing files in js
 const fs = require('fs');
 
-fs.writeFile('./docs/blog1.txt','hello world',()=>{
-    console.log('file was written');
+// fs.writeFile('./docs/blog1.txt','hello world',()=>{
+//     console.log('file was written');
 
-});
+// });
 
-fs.writeFile('./docs/blog2.txt','hello world',()=>{
-    console.log('file was written');
+// fs.writeFile('./docs/blog2.txt','hello world',()=>{
+//     console.log('file was written');
 
-});
+// });
 
-//Reading files in js
-fs.readFile('./docs/blog1.txt',(err,data)=>{
-    if(err){
-        console.log(err);
+// //Reading files in js
+// fs.readFile('./docs/blog1.txt',(err,data)=>{
+//     if(err){
+//         console.log(err);
   
-    }
-          if(data){
-            console.log(data.toString());
-        }
-});
-// Creating directories
+//     }
+//           if(data){
+//             console.log(data.toString());
+//         }
+// });
+// // Creating directories
 
-fs.mkdir('./gallary',(err)=>{
+// fs.mkdir('./gallary',(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('folder created');
+//     }
+// });
+
+
+//remove directory s
+
+// fs.rmdir('./gallary',(err)=>{
+//     if(err){
+//         console.log(err);
+//         else{
+//             console.log('folder deleted');
+//         };
+
+
+
+//Deleting files in a directory
+if(fs.existsSync('./docs/blog2.txt')) {
+fs.unlink('./docs/blog2.txt',(err)=>{
     if(err){
         console.log(err);
     }
     else{
-        console.log('folder created');
+        console.log('file deleted');
     }
 });
+}
+
+
+
