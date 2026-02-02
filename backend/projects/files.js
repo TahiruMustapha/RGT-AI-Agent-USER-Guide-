c96026ea44e3488b8232c3ab10f78e03f4d816a7
BraCoded02024 <owusubernard050931@gmail.com>
2026-02-02 10:05:28 +0000

    Reading and writing s files

--- backend/projects/files.js
+++ backend/projects/files.js
@@ -1,24 +1,25 @@
-//reading files
+//Writing files in js
 const fs = require('fs');
+
+fs.writeFile('./docs/blog1.txt','hello world',()=>{
+    console.log('file was written');
+
+});
+
+fs.writeFile('./docs/blog2.txt','hello world',()=>{
+    console.log('file was written');
+
+});
+
+//Reading files in js
 fs.readFile('./docs/blog1.txt',(err,data)=>{
-
-if(err){
-console.log(err);
-}
-console.log(data.toString());
-
-})
-
-
-
-
-//writing files
+    if(err){
+        console.log(err);
+  
+    }
+          if(data){
+            console.log(data.toString());
+        }
+});
 
 
-//directories
-
-
-
-
-
-//deleting files a
