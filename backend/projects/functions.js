1b10e8b1886418e3fc6711e9f3f6a9bb1f033153
BraCoded02024 <owusubernard050931@gmail.com>
2026-02-02 16:45:55 +0000

    Funnction Declaration , and with parameters

---
+++ backend/projects/functions.js
@@ -0,0 +1,12 @@
+// function makepizza(){
+// console.log('pizza is made');
+
+// }
+
+// makepizza();
+
+function bakeCookies(type, quantity) {
+    console.log(`Baking ${quantity} ${type} cookies...`);
+}
+
+bakeCookies("chocolate chip", 12);
