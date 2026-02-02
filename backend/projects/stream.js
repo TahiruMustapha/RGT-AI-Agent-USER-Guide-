59f1ced727f262a8f86624f49bbdff58dfccddea
BraCoded02024 <owusubernard050931@gmail.com>
2026-02-02 15:14:16 +0000

    Fix stream.js syntax errors and add blog2.txt

--- backend/projects/stream.js
+++ backend/projects/stream.js
@@ -1,16 +1,17 @@
 const fs= require('fs');
 
 const readStream = fs.createReadStream('./docs/blog1.txt', {encoding:'utf8'});
-
+const writeStream = fs.createWriteStream('./docs/blog2.txt');
 readStream.on('data',( chunk)=>{
 
     console.log('-----NEW CHUNK-----');
 console.log(chunk);
+// writeStream.write('\nNEW CHUNK\n');
+// writeStream.write(chunk);
 
-}
-);
+});
 
 
-
+readStream.pipe(writeStream);
 
 
